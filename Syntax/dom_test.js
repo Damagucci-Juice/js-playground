77187e1950a77799075ea4795f2ccc00fcd2f806
Damagucci-Juice <jsr9874@gmail.com>
2020-12-12 04:47:34 +0900

    Dislocate javascript, DOM and HTML.
    DOM과 HTML에 대해서 배웠다.
    문서를 객체를 이용해서 계층 구조로 표현함.
    .html 파일과 .js 파일로 분리하니 코드가 깔끔해진다.

---
+++ Syntax/dom_test.js
@@ -0,0 +1,7 @@
+const list = document.getElementsByTagName('h1');
+list[0].innerHTML;
+list[0].innerHTML = "Hi"
+list[1].innerHTML = "Here is second H1 tag"
+
+const list2 = document.getElementById('main');
+list2.innerHTML = "Hell";
