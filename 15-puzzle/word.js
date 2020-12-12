50ef6ad301f2275e161edf68582ba659fc613ce6
Damagucci-Juice <jsr9874@gmail.com>
2020-12-12 20:35:31 +0900

    Refactorying 15-puzzle
    코드는 testcode 부터 짜는게 좋다는데 감을 못잡겠다.

--- 15-puzzle/word.js
+++ 15-puzzle/word.js
@@ -1,33 +1,91 @@
-var str = document.getElementById('word1').innerHTML;
-var word2 = document.getElementById('word2');
+//html elements
+var word1 = document.getElementById('word1');   //answer
+var word2 = document.getElementById('word2');   //buttons
+var check = document.getElementById('check');   // word1 === word2?
 
-var game = {};
-game.word = str.split('');
-game.btns = [];
 
-for (var i = 0; i < str.length; i++) {
-    var btn = document.createElement('button');
-    btn.innerHTML = str[i];         // string 이여도 배열의 원소 읽어 오는 것처럼 각 글자를 읽어 올 수 있음.
-    word2.appendChild(btn);
-    game.btns.push(btn);
-}
+//game objects
+var game = {'btns':[]};
+game.words = 'apple,linux,javascript,tutorial,damagucci,baby,girlfriend,legend'.split(',');
+// choose 1 word from words
+game.choose = function(){
+    var idx = Math.floor(Math.random() * this.words.length);
+    this.answer = this.words[idx];
+    this.letters = this.answer.split('');
+    word1.innerHTML = this.answer;
+};
 
-game.copyBtnText = function () {
-    for (var i = 0; i < this.word.length; i++) {
-        this.btns[i].innerHTML = this.word[i];
+game.addButtons = function() {
+    for (var i = 0; i < this.letters.length; i++) {
+        var btn = document.createElement('button');
+        btn.innerHTML = this.answer[i];         // string 이여도 배열의 원소를 읽어 오는 것처럼 각 글자를 읽어 올 수 있음.
+        word2.appendChild(btn);
+        this.btns.push(btn);
     }
 };
 
-var swap = function (event) {
+game.updateDisplay = function () {
+    var gameStr = this.letters.join(''); 
+    if (this.answer === gameStr) {
+        check.innerHTML = '일치합니다.';
+    } else {
+        check.innerHTML = '일치하지 않습니다.';
+    }
+};
 
+game.init = function() {
+    this.choose();
+    this.addButtons();
+    this.updateDisplay();
+}
+game.init();
+
+game.copyBtnText = function () {
+    for (var i = 0; i < this.letters.length; i++) {
+        this.btns[i].innerHTML = this.letters[i];
+    }
 };
-var rshift = function (event) {
-    const s = game.word.pop();
-    game.word.unshift(s);
+
+//event handler for swap button
+var swap = function () {
+    var temp = [];
+    //copy and swap
+    while (game.letters.length != 0) {
+        var s = game.letters.pop();
+        temp.push(s);
+    }
+    game.letters = temp;
     game.copyBtnText();
+    game.updateDisplay();
 };
-var lshift = function (event) {
-    const s = game.word.shift();
-    game.word.push(s);
+
+var rshift = function () {
+    const s = game.letters.pop();
+    game.letters.unshift(s);
     game.copyBtnText();
+    game.updateDisplay();
 };
+
+var lshift = function () {
+    const s = game.letters.shift();
+    game.letters.push(s);
+    game.copyBtnText();
+    game.updateDisplay();
+};
+
+
+//shuffle
+game.shuffle = function() {
+    var toggle = Math.floor(Math.random() * 2) === 0;
+
+    if (toggle) {
+        swap();
+    }
+
+    var n = Math.floor(Math.random() * game.answer.length);
+
+    for (var i = 0; i < n; i++) {
+        rshift();
+    }
+};
+game.shuffle();
