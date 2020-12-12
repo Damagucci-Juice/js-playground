f6331e28e46e4db66456eb7a6e91635460230ad5
Damagucci-Juice <jsr9874@gmail.com>
2020-12-13 01:35:34 +0900

    Add time. Fin2

--- 16-puzzle/word.js
+++ 16-puzzle/word.js
@@ -1,11 +1,22 @@
+//html elements
 var word1 = document.getElementById('word1');   //answer
 var word2 = document.getElementById('word2');   //button
 var check = document.getElementById('check');   // word1 === word2?
+var progress = document.getElementById('progress');  //progress check
+var time = document.getElementById('time');
 
 
+//gmae objects
+var game = {
+    'btns': [],
+    'maxPlay': 3,
+    'current': 0
+};
 
-var game = {'btns': []};
+game.startTime = Date.now();
+
 game.words = 'apple,book,condition,monitor,workplace,button,shoes,baby,damagucci,legend,girlfriend'.split(',');
+
 game.choose = function() {
     var idx = Math.floor(Math.random() * this.words.length);
     this.answer = this.words[idx];
@@ -20,15 +31,26 @@ game.addButtons = function() {
         this.btns.push(btn);
     }
 };
+game.removeButtons = function() {
+    for (var i = 0; i < this.btns.length; i++) {
+        word2.removeChild(this.btns[i]);
+    }
+    this.btns = [];
+};
+
+game.checkGood = function() {
+    return this.answer === this.letters.join('');
+};
 
 game.updateDisplay = function() {
     var gameStr = this.letters.join('');
-    if (this.answer === gameStr) {
+    if (this.checkGood()) {
         check.innerHTML = '일치합니다.';
     } else {
         check.innerHTML = '일치하지 않습니다.';
     }
 };
+
 game.init = function() {
     this.choose();
     this.addButtons();
@@ -43,28 +65,65 @@ game.copyBtnText = function () {
 };
 
 //event handler for swap button
-var swap = function () {
+game.swap = function () {
     var temp = [];
     //copy and swap
-    while (game.letters.length != 0){
-        let x = game.letters.pop();
+    while (this.letters.length != 0){
+        let x = this.letters.pop();
         temp.push(x);
     }
-    game.letters = temp;
-    game.copyBtnText();
-    game.updateDisplay();
+    this.letters = temp;
+    this.copyBtnText();
+    this.updateDisplay();
 };
-var rshift = function () {
-    const s = game.letters.pop();
-    game.letters.unshift(s);
-    game.copyBtnText();
-    game.updateDisplay();
+game.rshift = function () {
+    const s = this.letters.pop();
+    this.letters.unshift(s);
+    this.copyBtnText();
+    this.updateDisplay();
 };
+game.lshift = function () {
+    const s = this.letters.shift();
+    this.letters.push(s);
+    this.copyBtnText();
+    this.updateDisplay();
+};
+
+game.progress = function() {
+    if (game.checkGood()) {
+        game.current++;
+        game.removeButtons();
+        game.init();
+        game.shuffle();
+        var str = "";
+        for (var i = 0; i < game.current; i++) {
+            str += "O";
+        }
+        progress.innerHTML = str;
+    }
+
+    if (game.current == game.maxPlay) {
+        var sec = (Date.now() - game.startTime) /1000;
+        alert("Good! Your Record: " + sec + " sec");
+        clearInterval(x);
+        check.innerHTML = "Thanks for playing";
+    }
+};
+
+//event handler for swap button
+var swap = function () {
+    game.swap();
+    game.progress();
+};
+
 var lshift = function () {
-    const s = game.letters.shift();
-    game.letters.push(s);
-    game.copyBtnText();
-    game.updateDisplay();
+    game.lshift();
+    game.progress();
+};
+
+var rshift = function () {
+    game.rshift();
+    game.progress();
 };
 
 //shuffle
@@ -72,12 +131,20 @@ game.shuffle = function() {
     var toggle = Math.floor(Math.random() * 2) === 0;
 
     if(toggle) {
-        swap();
+        this.swap();
     }
 
-    var n = Math.floor(Math.random() * (game.answer.length -1 ));
+    var rmax = Math.max(game.answer.length - 2, 1);
+    var n = Math.floor(Math.random() * rmax) + 1;
     for (var i = 0; i < n; i++) {
-        rshift();
+        this.rshift();
     }
 };
 game.shuffle();
+
+var updateTime = function() {
+    var now = Date.now() - game.startTime;
+    time.innerHTML = (now / 1000) + " s";
+}
+
+var x = setInterval(updateTime, 50);
